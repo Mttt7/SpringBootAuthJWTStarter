f5a834cb8cc6fba41c72dbe25e5095904cd32f97
Michal Tomaszewski <groobytt@gmail.com>
2024-10-31 19:03:06 +0100

    fix/ fix bug

--- jwtstarter/src/main/java/com/mt/jwtstarter/config/security/JWTAuthenticationFilter.java
+++ jwtstarter/src/main/java/com/mt/jwtstarter/config/security/JWTAuthenticationFilter.java
@@ -5,7 +5,10 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,12 +16,11 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+@NoArgsConstructor
+@AllArgsConstructor
 public class JWTAuthenticationFilter extends OncePerRequestFilter {
 
-    @Autowired
     private JWTGenerator tokenGenerator;
-
-    @Autowired
     private CustomUserDetailsService customUserDetailsService;
 
     @Override
